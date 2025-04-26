9a656aca5f550c20d309128ee8fdef349c7baffd
Hugo Soares <hugoferreirasoares@gmail.com>
2025-04-26 16:45:52 +0100

    feat: new v2

--- includes/client.h
+++ includes/client.h
@@ -8,16 +8,6 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include "common.h"
 
-// Client function prototypes
-void send_request_and_get_response(Request *req, Response *resp);
-void add_document(const char *title, const char *authors, const char *year, const char *path);
-void query_document(const char *key);
-void delete_document(const char *key);
-void count_lines(const char *key, const char *keyword);
-void search_documents(const char *keyword, const char *nr_processes);
-void shutdown_server();
 
 #endif /* CLIENT_H */
-
--- includes/server.h
+++ includes/server.h
@@ -12,24 +12,11 @@
 #include <errno.h>
 #include <signal.h>
 #include <time.h>
-#include "common.h"
-#include "document_operations.h"
-#include "cache_management.h"
-#include "search_operations.h"
-#include "metadata_management.h"
 
-// Server function prototypes
-void initialize_server();
-void cleanup_server();
-void handle_client_requests();
-void process_request(Request *req, Response *resp);
-void handle_shutdown(Response *resp);
-void handle_signal(int sig);
 
 // Global variables
 extern char document_folder[256];
 extern int cache_size;
-extern Document *documents;
 extern int num_documents;
 extern int next_key;
 extern int server_running;
