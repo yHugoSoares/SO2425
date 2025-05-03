d1eabdd6574aa027743ff6e39fb3d96908d8a7cb
franciscormartins <kiko.ribeiro.martins@gmail.com>
2025-05-03 19:50:48 +0100

    add handler request

--- src/handle_request.c
+++ src/handle_request.c
@@ -1,133 +1,133 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include "common.h"
-#include "cache.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <sys/stat.h>
+// #include <sys/types.h>
+// #include "common.h"
+// #include "cache.h"
 
-Metadata metadata;
-char *document_folder;
+// Metadata metadata;
+// char *document_folder;
 
 
-// NOT FULLY CORRECT
-void save_metadata() {
-    int fd = open(METADATA_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-    if (fd == -1) {
-        perror("save_metadata");
-        return;
-    }
+// // NOT FULLY CORRECT
+// void save_metadata() {
+//     int fd = open(METADATA_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+//     if (fd == -1) {
+//         perror("save_metadata");
+//         return;
+//     }
 
-    ssize_t written = write(fd, &metadata, sizeof(metadata));
-    if (written != sizeof(metadata)) {
-        perror("Error writing metadata");
-    }
+//     ssize_t written = write(fd, &metadata, sizeof(metadata));
+//     if (written != sizeof(metadata)) {
+//         perror("Error writing metadata");
+//     }
 
-    close(fd);
-}
+//     close(fd);
+// }
 
-void load_metadata() {
-    int fd = open(METADATA_FILE, O_RDONLY);
-    if (fd == -1) {
-        metadata.count = 0;
-        metadata.last_id = 0;
-        return;
-    }
-    read(fd, &metadata, sizeof(Metadata));
-    close(fd);
-}
+// void load_metadata() {
+//     int fd = open(METADATA_FILE, O_RDONLY);
+//     if (fd == -1) {
+//         metadata.count = 0;
+//         metadata.last_id = 0;
+//         return;
+//     }
+//     read(fd, &metadata, sizeof(Metadata));
+//     close(fd);
+// }
 
-int handle_operation(int fd_server) {
-    MensagemCliente pedido;
+// int handle_operation(int fd_server) {
+//     MensagemCliente pedido;
     
-    while (read(fd_server, &pedido, sizeof(MensagemCliente)) > 0) {
-        printf("Processing: %c\n", pedido.operacao);
-        char response[MAX_DADOS] = {0};
-        int client_fd;
+//     while (read(fd_server, &pedido, sizeof(MensagemCliente)) > 0) {
+//         printf("Processing: %c\n", pedido.operacao);
+//         char response[MAX_DADOS] = {0};
+//         int client_fd;
 
-        switch(pedido.operacao) {
-            case 'a': { // ADD INDEXATION
-                printf("Adding document...\n");
-                Document doc;
-                char title[200], authors[200], year[5], path[64];
+//         switch(pedido.operacao) {
+//             case 'a': { // ADD INDEXATION
+//                 printf("Adding document...\n");
+//                 Document doc;
+//                 char title[200], authors[200], year[5], path[64];
 
-                sscanf(pedido.dados, "%[^;];%[^;];%[^;];%[^;]", title, authors, year, path);
-                printf("Title: %s\nAuthors: %s\nYear: %s\nPath: %s\n", title, authors, year, path);
+//                 sscanf(pedido.dados, "%[^;];%[^;];%[^;];%[^;]", title, authors, year, path);
+//                 printf("Title: %s\nAuthors: %s\nYear: %s\nPath: %s\n", title, authors, year, path);
                 
-                // Basic ID generation
-                doc.id = ++metadata.last_id;
-                strncpy(doc.title, title, sizeof(doc.title));
-                strncpy(doc.authors, authors, sizeof(doc.authors));
-                strncpy(doc.year, year, sizeof(doc.year));
-                strncpy(doc.path, path, sizeof(doc.path));
+//                 // Basic ID generation
+//                 doc.id = ++metadata.last_id;
+//                 strncpy(doc.title, title, sizeof(doc.title));
+//                 strncpy(doc.authors, authors, sizeof(doc.authors));
+//                 strncpy(doc.year, year, sizeof(doc.year));
+//                 strncpy(doc.path, path, sizeof(doc.path));
 
 
-                metadata.docs[metadata.count++] = doc;
-                printf("Document added with ID: %d\n", doc.id);
-                save_metadata();
-                printf("Metadata saved.\n");
-                snprintf(response, sizeof(response), "Added ID %d", doc.id);
-                break;
-            }
+//                 metadata.docs[metadata.count++] = doc;
+//                 printf("Document added with ID: %d\n", doc.id);
+//                 save_metadata();
+//                 printf("Metadata saved.\n");
+//                 snprintf(response, sizeof(response), "Added ID %d", doc.id);
+//                 break;
+//             }
             
-            case 'c': { // CHECK WITH KEY (STILL NOT WORKING)
-                int doc_id;
-                if(sscanf(pedido.dados, "%d", &doc_id) != 1) {
-                    strcpy(response, "Invalid ID");
-                    break;
-                }
+//             case 'c': { // CHECK WITH KEY (STILL NOT WORKING)
+//                 int doc_id;
+//                 if(sscanf(pedido.dados, "%d", &doc_id) != 1) {
+//                     strcpy(response, "Invalid ID");
+//                     break;
+//                 }
 
-                for(int i = 0; i < metadata.count; i++) {
-                    if(metadata.docs[i].id == doc_id) {
-                        snprintf(response, sizeof(response),
-                                "Title: %s\nPath: %s",
-                                metadata.docs[i].title,
-                                metadata.docs[i].path);
-                        break;
-                    }
-                }
-                if(!response[0]) strcpy(response, "Not found");
-                break;
-            }
+//                 for(int i = 0; i < metadata.count; i++) {
+//                     if(metadata.docs[i].id == doc_id) {
+//                         snprintf(response, sizeof(response),
+//                                 "Title: %s\nPath: %s",
+//                                 metadata.docs[i].title,
+//                                 metadata.docs[i].path);
+//                         break;
+//                     }
+//                 }
+//                 if(!response[0]) strcpy(response, "Not found");
+//                 break;
+//             }
             
-            case 'd': { // DELETE WITH KEY
-                int doc_id;
-                if(sscanf(pedido.dados, "%d", &doc_id) != 1) {
-                    strcpy(response, "Invalid ID");
-                    break;
-                }
+//             case 'd': { // DELETE WITH KEY
+//                 int doc_id;
+//                 if(sscanf(pedido.dados, "%d", &doc_id) != 1) {
+//                     strcpy(response, "Invalid ID");
+//                     break;
+//                 }
 
-                for(int i = 0; i < metadata.count; i++) {
-                    if(metadata.docs[i].id == doc_id) {
-                        metadata.docs[i] = metadata.docs[--metadata.count];
-                        // save_metadata();
-                        snprintf(response, sizeof(response), "Removed ID %d", doc_id);
-                        break;
-                    }
-                }
-                if(!response[0]) strcpy(response, "Not found");
-                break;
-            }
+//                 for(int i = 0; i < metadata.count; i++) {
+//                     if(metadata.docs[i].id == doc_id) {
+//                         metadata.docs[i] = metadata.docs[--metadata.count];
+//                         // save_metadata();
+//                         snprintf(response, sizeof(response), "Removed ID %d", doc_id);
+//                         break;
+//                     }
+//                 }
+//                 if(!response[0]) strcpy(response, "Not found");
+//                 break;
+//             }
 
-            case 'l': {
-                // TODO
-            }
+//             case 'l': {
+//                 // TODO
+//             }
 
-            case 's': {
-                // TODO
-            }
+//             case 's': {
+//                 // TODO
+//             }
             
-            case 'f':
-                return 0; // Terminate server
-        }
+//             case 'f':
+//                 return 0; // Terminate server
+//         }
         
-        if((client_fd = open(pedido.resposta_fifo, O_WRONLY)) != -1) {
-            write(client_fd, response, strlen(response)+1);
-            close(client_fd);
-        }
-        printf("Done processing: %c\n", pedido.operacao);
-    }
-    return 1;
-}
+//         if((client_fd = open(pedido.resposta_fifo, O_WRONLY)) != -1) {
+//             write(client_fd, response, strlen(response)+1);
+//             close(client_fd);
+//         }
+//         printf("Done processing: %c\n", pedido.operacao);
+//     }
+//     return 1;
+// }
