42767d949509e2a07c8d0fe45c5416742eea3112
franciscormartins <kiko.ribeiro.martins@gmail.com>
2025-05-07 18:12:03 +0100

    add funcaoes no ponto h

--- includes/handle_request.h
+++ includes/handle_request.h
@@ -18,6 +18,12 @@ extern char *document_folder;
 // Function declarations
 void save_metadata();
 void load_metadata();
+int handle_shutdown(MensagemCliente pedido);
+int handle_add(MensagemCliente pedido);
+int handle_consulta(MensagemCliente pedido);
+int handle_remove(MensagemCliente pedido);
+// int handle_lines_number(MensagemCliente pedido, const char *document_folder);
+// int handle_search(MensagemCliente pedido, const char *document_folder);
 int handle_request(MensagemCliente pedido, const char *document_folder);
 
 #endif /* HANDLE_REQUEST_H */
